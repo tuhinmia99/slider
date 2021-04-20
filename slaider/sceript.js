10f694038a783259d2a5a3cc17dea13590d2b886
tuhinmia99 <63042100+tuhinmia99@users.noreply.github.com>
2021-04-21 01:45:17 +0600

    Add files via upload

---
+++ slaider/sceript.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+    $('.slider1').slick({
+        infinite: true,
+        //show img display
+        slidesToShow: 1,
+        //how much img scorinong
+        slidesToScroll: 1,
+        //next prives button
+        arrows:true,
+        //auto play
+        autoplay: true,
+        //autoplay speed
+        autoplaySpeed: 2000,
+        //smouth
+        fade: true,
+        //movement sperit
+        speed: 500,
+        //1.2.3 This is dot
+        dots: false,
+        //left button
+        prevArrow: '.arrow-left',
+        //right button
+        nextArrow: '.arrow-right',
+
+ });
+     
+})
+
+/*//Slider 2
+
+$(document).ready(function(){
+
+    $('.slider2pro').slick({
+        infinite: true,
+        //show img display
+        slidesToShow: 3,
+        //how much img scorinong
+        slidesToScroll: 1,
+        //next prives button
+        arrows:true,
+        //auto play
+        autoplay: true,
+        //movement sperit
+        speed: 500,
+        //1.2.3 This is dot
+        dots: false,
+        //auto play
+        autoplay: true,
+        //autoplay speed
+        autoplaySpeed: 2000,
+
+ });
+     
+})*/
